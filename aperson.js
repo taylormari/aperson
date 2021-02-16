db1f83a74b3ef47d173f046643416f051a63060b
taylormari <tpannell@risd.edu>
2021-02-16 14:11:40 -0800

    coolio

---
+++ aperson.js
@@ -0,0 +1,19 @@
+ $(function(){
+	$("#social").draggable();
+})
+
+ $(function(){
+	$("#mask").draggable();
+})
+
+ $(function(){
+	$("#body").draggable();
+})
+
+ $(function(){
+	$("#mind").draggable();
+})
+
+ $(function(){
+	$("#will").draggable();
+})
